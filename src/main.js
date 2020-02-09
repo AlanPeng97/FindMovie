42fb01f8b9476e898f76096bab8af6f402a93c97
yucheng <a15370023991@outlook.com>
2020-02-09 16:00:01 +0000

    配置vuetify

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuetify from 'vuetify'
+import vuetify from './plugins/vuetify'
 
+Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 new Vue({
+  vuetify,
   router,
   store,
   render: h => h(App)
