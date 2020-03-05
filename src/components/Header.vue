5375e37087828eb7f06357d3f7ee27f281dec39d
yucheng <a15370023991@outlook.com>
2020-03-05 01:09:44 +0000

    搜索输入框判定输入是否空白

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,10 +6,11 @@
       <div id="tf">
       <v-text-field
       label="Search"
+      v-model.trim="name"
       outlined
       ></v-text-field>
       </div>
-      <v-btn icon>
+      <v-btn icon @click="checkSearch">
         <v-icon>fa-search</v-icon>
       </v-btn>
       <v-spacer></v-spacer>
@@ -18,7 +19,7 @@
         <v-icon>$vuetify.icons.Account</v-icon>
       </v-btn>
 
-      <v-btn to='/' icon>
+      <v-btn to='/' icon color="primary">
         <v-icon>$vuetify.icons.Home</v-icon>
       </v-btn>
     </v-app-bar>
@@ -28,7 +29,19 @@
 <script scoped>
 export default {
   data: () => ({
-  })
+    name: '',
+    searchblank: true
+  }),
+  methods: {
+    checkSearch () {
+      if (this.name !== '') {
+        this.searchblank = false
+      } else {
+        this.searchblank = true
+        alert('Please enter your search')
+      }
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -4,14 +4,17 @@ import router from './router'
 import store from './store'
 import Vuetify from 'vuetify'
 import vuetify from './plugins/vuetify'
+import axios from '../node_modules/axios'
 
 Vue.use(Vuetify)
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios
 
 new Vue({
   el: '#app',
   vuetify,
   router,
   store,
+  axios,
   render: h => h(App)
 }).$mount('#app')
